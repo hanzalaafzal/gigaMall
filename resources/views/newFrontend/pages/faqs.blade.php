c125d86cec2befe3350f92cdfe4525357730a75f
malikhunzala1337@gmail.com <48134105+hanzalaafzal@users.noreply.github.com>
2021-12-28 20:09:06 +0500

    faqs

--- resources/views/newFrontend/pages/faqs.blade.php
+++ resources/views/newFrontend/pages/faqs.blade.php
@@ -1,13 +1,14 @@
 @extends('newFrontend.layout')
 
+@push('css')
+
+@endpush
+
 @section('main_content')
 <div class="ps-page--single">
     <div class="ps-breadcrumb">
         <div class="container">
-            <ul class="breadcrumb">
-                <li><a href="index.html">Home</a></li>
-                <li>Frequently Asked Questions</li>
-            </ul>
+
         </div>
     </div>
     <div class="ps-faqs">
@@ -16,109 +17,159 @@
                 <h1>Frequently Asked Questions</h1>
             </div>
             <div class="ps-section__content">
-                <div class="table-responsive">
+                <div class="table-responsive cust-table">
                     <table class="table ps-table--faqs">
                         <tbody>
                             <tr>
-                                <td class="heading" rowspan="3">
+                                <td class="heading">
                                     <h4>ABOUT US</h4>
                                 </td>
-                                <td class="question"> Why E-Bazarr ?</td>
-                                <td>E-BAZARR is an ecommerce website which provides its customers to buy and sell thousands of products online. E-BAZARR connects sellers with the customers and provides thousands of products to its customers within a click.</td>
+                            </tr>
+                            <tr>
+                              <td class="question"> Why E-Bazarr ?</td>
+                            </tr>
+                            <tr>
+                              <td>E-BAZARR is an ecommerce website which provides its customers to buy and sell thousands of products online. E-BAZARR connects sellers with the customers and provides thousands of products to its customers within a click.</td>
                             </tr>
 
                         </tbody>
                         <tbody>
                             <tr>
-                                <td class="heading" rowspan="3">
+                                <td class="heading" rowspan="1">
                                     <h4>Affiliate Program</h4>
                                 </td>
-                                <td class="question"> What is Affiliate Program?</td>
-                                <td>Affiliate program is the process by which an affiliator can earn a commision for marketing e-bazarr's products. Following are the points affiliator needs to follow:
-                                  <ul>
-                                    <li>The affiliate simply searches for a product.</li>
-                                    <li>Creates its own affiliate product link from affiliate page.</li>
-                                    <li>Promotes that product and earns a piece of the profit from each sale affiliator makes.</li>
-                                    <li>Every product has its own profit percentage.</li>
-                                  </ul>
-                                </td>
                             </tr>
                             <tr>
-                                <td class="question"> How to get started with affiliate marketing?</td>
-                                <td>
-                                  <ul>
-                                    <li>Choose your product to sale.</li>
-                                    <li>Create your own affiliate link.</li>
-                                    <li>Drive traffic to your affiliate site.</li>
-                                    <li>Get clicks on your affiliate links.</li>
-                                    <li>Convert clicks to sales.</li>
-                                  </ul>
-                                </td>
+                              <td class="question"> What is Affiliate Program?</td>
+
                             </tr>
                             <tr>
+
+                              <td>Affiliate program is the process by which an affiliator can earn a commision for marketing e-bazarr's products. Following are the points affiliator needs to follow:
+                                <ul>
+                                  <li>The affiliate simply searches for a product.</li>
+                                  <li>Creates its own affiliate product link from affiliate page.</li>
+                                  <li>Promotes that product and earns a piece of the profit from each sale affiliator makes.</li>
+                                  <li>Every product has its own profit percentage.</li>
+                                </ul>
+                              </td>
+                            </tr>
+                            <tr>
+                              <td rowspan="1"></td>
+                            </tr>
+                            <tr>
+                              <td class="question"> How to get started with affiliate marketing?</td>
+
+                            </tr>
+                            <tr>
+
+                              <td>
+                                <ul>
+                                  <li>Choose your product to sale.</li>
+                                  <li>Create your own affiliate link.</li>
+                                  <li>Drive traffic to your affiliate site.</li>
+                                  <li>Get clicks on your affiliate links.</li>
+                                  <li>Convert clicks to sales.</li>
+                                </ul>
+                              </td>
+                            </tr>
+                            <tr>
+
                                 <td class="question">How much do affiliate link pay?</td>
-                                <td>
-                                  Affiliate links for affiliators are one of the greatest sources of supplementary income. It allows them to generate up to 5,000 to 500,000 monthly from promoting helpful products. Substantial income is possible if affilates are able to reach out to a large audience that will listen to their voice.
-                                </td>
+
+                            </tr>
+                            <tr>
+
+                              <td >
+                                Affiliate links for affiliators are one of the greatest sources of supplementary income. It allows them to generate up to 5,000 to 500,000 monthly from promoting helpful products. Substantial income is possible if affilates are able to reach out to a large audience that will listen to their voice.
+                              </td>
                             </tr>
 
                         </tbody>
                         <tbody>
                             <tr>
-                                <td class="heading" rowspan="3">
+                                <td class="heading" rowspan="1">
                                     <h4>OUR PRODUCTS</h4>
                                 </td>
-                                <td class="question"> What products do we offer?</td>
-                                <td>E-BAZARR product include books, apparel, baby products, consumer electronics, beauty products, health and personal-care items, industrial & scientific supplies, technology tools, kitchen items, jewelry and watches, lawn and garden items, musical instruments, sporting goods, tools, arts and crafts, automotive items and toys/games.</td>
+                            </tr>
+                            <tr>
+                              <td class="question">What products do we offer?</td>
+                            </tr>
+                            <tr>
+                              <td>E-BAZARR product include books, apparel, baby products, consumer electronics, beauty products, health and personal-care items, industrial & scientific supplies, technology tools, kitchen items, jewelry and watches, lawn and garden items, musical instruments, sporting goods, tools, arts and crafts, automotive items and toys/games.</td>
                             </tr>
                             <tr>
 
                                 <td class="question">How are the quality of the products?</td>
-                                <td>E-BAZARR enforces ban on sellers who violate our selling policies. Violations related to product authenticity are categorized as intellectual property violations. Issues with the overall product quality, including products that do not match their descriptions, can be categorized as product condition violations. Hence, E-BAZARR ensures every product is at high quality standards.</td>
+                            </tr>
+                            <tr>
+
+                              <td>E-BAZARR enforces ban on sellers who violate our selling policies. Violations related to product authenticity are categorized as intellectual property violations. Issues with the overall product quality, including products that do not match their descriptions, can be categorized as product condition violations. Hence, E-BAZARR ensures every product is at high quality standards.</td>
                             </tr>
                             <tr>
 
                                 <td class="question">What is the return policy for products?</td>
-                                <td>Items shipped from E-BAZARR, can be returned within 7 days after receiving product. It can take up to 10 days for an item to reach us once you return it. Once the item is received at our fulfillment center, it takes 7 days for the refund to be processed and 3-5 business days for the refund amount to show up in your account.</td>
                             </tr>
+                            <tr>
 
+                              <td>Items shipped from E-BAZARR, can be returned within 7 days after receiving product. It can take up to 10 days for an item to reach us once you return it. Once the item is received at our fulfillment center, it takes 7 days for the refund to be processed and 3-5 business days for the refund amount to show up in your account.</td>
+                            </tr>
                         </tbody>
                         <tbody>
                             <tr>
-                                <td class="heading" rowspan="3">
+                                <td class="heading" rowspan="1">
                                     <h4>PAYMENT METHODS</h4>
                                 </td>
-                                <td class="question">Which payment method do you offer?</td>
-                                <td>E-BAZARR offers three types of payment methods:
-                                  <ul>
-                                    <li>Through Credit Card </li>
-                                    <li>Through Bank Transfer / Easy-Paisa/ Jazz Cash</li>
-                                    <li>Cash on delivery</li>
-                                  </ul>
-                                </td>
+                            </tr>
+                            <tr>
+                              <td class="question">Which payment method do you offer?</td>
+
                             </tr>
                             <tr>
 
+                              <td>E-BAZARR offers three types of payment methods:
+                                <ul>
+                                  <li>Through Credit Card </li>
+                                  <li>Through Bank Transfer / Easy-Paisa (soon)/ Jazz Cash (soon)</li>
+                                  <li>Cash on delivery</li>
+                                </ul>
+                              </td>
+                            </tr>
+                            <tr>
                                 <td class="question">Is online payment secure?</td>
-                                <td>Yes, Payment methods implemented in our website E-BAZARR are 100 % secure.</td>
+                            </tr>
+                            <tr>
+
+                              <td>Yes, Payment methods implemented in our website E-BAZARR are 100 % secure.</td>
+
                             </tr>
 
                         </tbody>
                         <tbody>
                             <tr>
-                                <td class="heading" rowspan="3">
+                                <td class="heading" rowspan="1">
                                     <h4>DELIVERY SYSTEM (METHOD DAYS POLICY)</h4>
                                 </td>
-                                <td class="question">Do delivery of product follow covid protocols?</td>
-                                <td>Yes, our delivery of products does follow covid protocols.
-                                </td>
+
+                            </tr>
+                            <tr>
+                              <td class="question">Do delivery of product follow covid protocols?</td>
+
+                            </tr>
+                            <tr>
+
+                              <td>Yes, our delivery of products does follow covid protocols.
+                              </td>
                             </tr>
                             <tr>
 
                                 <td class="question">In how many days I will receive my parcel?</td>
-                                <td>Usually parcels or products are receivable within 3-5 working days, once dispatched.</td>
                             </tr>
+                            <tr>
 
+                              <td>Usually parcels or products are receivable within 3-5 working days, once dispatched.</td>
+
+                            </tr>
                         </tbody>
                     </table>
                 </div>
