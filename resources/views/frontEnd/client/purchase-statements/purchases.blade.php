8a3016787b86e4ea065932e13fa88edc5bbda75e
malikhunzala1337@gmail.com <48134105+hanzalaafzal@users.noreply.github.com>
2021-12-29 12:06:11 +0500

    rs

--- resources/views/frontEnd/client/purchase-statements/purchases.blade.php
+++ resources/views/frontEnd/client/purchase-statements/purchases.blade.php
@@ -43,10 +43,10 @@
                 <p>{{$order['total']}}</p>
             </div>
             <div class="purchase-item-date">
-                <p>{{$order['spend']}}</p>
+                <p>{{$order['spend']}} Rs</p>
             </div>
             <div class="purchase-item-date">
-                <p>{{round($order['ave-price'],2)}}</p>
+                <p>{{round($order['ave-price'],2)}} Rs</p>
             </div>
         </div>
       </div>
